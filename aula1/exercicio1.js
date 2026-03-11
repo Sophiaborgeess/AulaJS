6c4ee3502bd5421295213d6014a55c4c01022c76
Sophiaborgeess <sophiaaborgeess@gmail.com>
2026-03-11 14:51:42 -0300

    11/03

--- aula1/exercicio1.js
+++ aula1/exercicio1.js
@@ -4,5 +4,8 @@ var numero = prompt("digite um numero qualquer: ")
 
 var quadrado = Number(numero) * Number(numero);
 
-document.write(" <p> o numero digitado e" +numero+ "</p>")
-document.write
+document.write(" <p> o numero digitado é " + numero + "</p>")
+document.write("<p> o quadrado do numero digitado é " + quadrado + "</p>");
+
+var cubo = Number(numero) * Number(numero) * Number(numero);
+document.write("<p>0 cubo do número digitado é " + cubo + "</p>");
--- exercicio1.js
+++
@@ -1,8 +0,0 @@
-document.white("<h1 style='color : red'> Aula 01 -Exercício 1</h1>");
-
-var numero = prompt("Digite 1 número qualquer: ");
-
-document.white("<p>O numero digitado foi " + numero +"</p>");
-
-var quadrado =Number(numero) * Number(numero);
-document
