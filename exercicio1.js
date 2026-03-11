8e15fcce6f85bcb4a62a108df2f6d7a7a5eaeb8d
Sophiaborgeess <sophiaaborgeess@gmail.com>
2026-03-11 14:24:32 -0300

    11/03

---
+++ exercicio1.js
@@ -0,0 +1,8 @@
+document.white("<h1 style='color : red'> Aula 01 -Exercício 1</h1>");
+
+var numero = prompt("Digite 1 número qualquer: ");
+
+document.white("<p>O numero digitado foi " + numero +"</p>");
+
+var quadrado =Number(numero) * Number(numero);
+document
