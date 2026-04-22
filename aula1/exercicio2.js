9bb28e82a5738e692b20bd5d62f91a9dc17cfd02
Sophiaborgeess <sophiaaborgeess@gmail.com>
2026-04-22 15:12:37 -0300

    1212

--- aula1/exercicio2.js
+++
@@ -1,8 +0,0 @@
-document.write("<h1>Exercício 2</h1>");
-
-var temperatura = prompt("Digite a temperatura Fahrenheit");
-
-document.write("<p>A temperatura digitada foi " + temperatura + "</p>")
-
-var resultado = ( Number(temperatura) - 32) * 5/9;
-document.write("<p>O resultado convertido é " + resultado + "</p>" );
--- aula1/script.js
+++
@@ -1,9 +0,0 @@
-var nome = "Sophia"
-
-var idade = 16;
-
-var altura = 1.70
-
-console.log( nome )
-console.log( idade )
-console.log( altura )
